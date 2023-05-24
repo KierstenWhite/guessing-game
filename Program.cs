836bfa6f5f41b6fa4d7616fac7f23b1feae2d86c
Kiersten White <kierstenswhite@gmail.com>
2023-05-24 11:24:00 -0400

    Phase 2

--- Program.cs
+++ Program.cs
@@ -17,7 +17,19 @@ void Main()
             }
 
             int parsedUserGuess = int.Parse(userGuess);
-            Console.WriteLine($@"You guessed: {userGuess}"); //Display the user's guess back to the screen.
+            // Console.WriteLine($@"You guessed: {userGuess}"); //Display the user's guess back to the screen. ; No longer display the user's guess. They know what they guessed, right?
+
+            int secretNumber = 42; //Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
+
+            //Compare the user's guess with the secret number.
+            if (parsedUserGuess == secretNumber) {
+                Console.WriteLine("You guessed the secret number - 42!"); //Display a success message if the guess is correct,
+                break; //added a line so the game ends
+            }
+            else
+            {
+                Console.WriteLine("FAILURE."); //otherwise display a failure message.
+            }
         }
 }
 
@@ -34,6 +46,7 @@ Display a message to the user asking them to guess the secret number.
 Display a prompt for the user's guess.
 Take the user's guess as input and save it as a variable.
 Display the user's guess back to the screen.
+
 Phase 2
 The program should be updated to...
 
