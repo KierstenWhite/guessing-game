e252330b720184fd1d3adea725be5a52d868be1a
Kiersten White <kierstenswhite@gmail.com>
2023-05-25 09:26:00 -0400

    Phase 7...but it feels to Javascript-y

--- Program.cs
+++ Program.cs
@@ -5,15 +5,39 @@ void Main()
 {
         //Display a message to the user asking them to guess the secret number.
         Console.WriteLine("Welcome to Guessing Game - Can you guess the secret number?");
+        Console.WriteLine("Choose your difficulty level:");
+        Console.WriteLine("Press 1 for Easy.");
+        Console.WriteLine("Press 2 for Medium.");
+        Console.WriteLine("Press 3 for Hard.");
+        string difficulty = Console.ReadLine();
+        int parsedChosenDifficulty = int.Parse(difficulty);
+        int allowedTries = 0; //Give the user four chances to guess the number.
+        
+        if (parsedChosenDifficulty == 1) {
+            allowedTries = 8;
+        }
+        if (parsedChosenDifficulty == 2) {
+            allowedTries = 6;
+        }
+        else if (parsedChosenDifficulty == 3) {
+            allowedTries = 4;
+        };
 
-        int allowedTries = 4; //Give the user four chances to guess the number.
+        //Easy - this gives the user eight guesses.
+        //Medium - this gives the user six guesses.
+        //Hard - this gives the user four guesses.
+        
+        
+        Console.WriteLine("Guess any number!"); //Display a prompt for the user's guess.
+        
+       
         int numberOfTries = 0;
         int secretNumber = new Random().Next(0, 100); //Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number. ; Use a random number between 1 and 100 instead of a hard-coded number.
 
+    
 
         while (true)
         {
-            Console.WriteLine("Guess any number!"); //Display a prompt for the user's guess.
             string userGuess = Console.ReadLine(); //Take the user's guess as input and save it as a variable.
 
 
@@ -49,18 +73,6 @@ void Main()
 
 /*
 
-Phase 6
-The program should be updated to...
-
-Inform the user if their guess was too high or too low, when they guess incorrectly.
-Phase 7
-The program should be updated to...
-
-Prompt the user for a difficulty level before they are prompted to guess the number.
-The difficulty level should determine how many guesses the user gets. The difficulty levels should be:
-Easy - this gives the user eight guesses.
-Medium - this gives the user six guesses.
-Hard - this gives the user four guesses.
 Phase 8
 The program should be updated to...
 
